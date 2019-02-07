17c6cd57495c2bf7cad6f2ab8aa586ea96e1d197
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-02-07 15:58:45 -0500

    added short calculator unit tests

--- ShortCalculator.java
+++ ShortCalculator.java
@@ -1,26 +1,47 @@
-public class ShortCalculator {
+import java.util.Scanner;
 
-    public Short add(short baseValue, short difference) {
+public class ShortCalculator {
+    public static void main(String[] args) {
+        Short a = userInput();
+        Short b = userInput();
+        
+        add(a,b);
+        subtract(a,b);
+        multiply(a,b);
+        divide(a,b);
+        remainder(a,b);        
+    }
+    public static Short userInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number between 0 and 65535");
+        
+        Short n = scanner.nextShort();
+        return n;
+    }
+    
+
+
+    public static Short add(short baseValue, short difference) {
         Short sum = (short)(baseValue + difference);
         return sum;
     }
 
-    public Short subtract(short baseValue, short difference) {
+    public static Short subtract(short baseValue, short difference) {
         Short diff = (short)(baseValue - difference);
         return diff;
     }
 
-    public Short multiply(short multiplicand, short multiplier) {
+    public static Short multiply(short multiplicand, short multiplier) {
         Short product = (short)(multiplicand * multiplier);
         return product;
     }
 
-    public Short divide(short dividend, short divisor) {
+    public static Short divide(short dividend, short divisor) {
         Short division = (short)(dividend / divisor);
         return division;
     }
 
-    public Short remainder(short dividend, short divisor) {
+    public static Short remainder(short dividend, short divisor) {
         Short remainder = (short)(dividend % divisor);
         return remainder;
     }
--- ShortCalculatorTest.java
+++ ShortCalculatorTest.java
@@ -1,2 +1,68 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShortCalculatorTest {
+    private static volatile ShortCalculator calc = new ShortCalculator();
+    
+    @Test
+    public void testShortAddition() {
+        // : Given
+        short baseValue = 16384;
+        short addedValue = 7;
+        short expected = 16391;
+        // : When
+        short actual = ShortCalculator.add(baseValue, addedValue);
+        // : Then
+        assertEquals(expected, actual);
+    }
+    
+     @Test
+    public void testShortSubtraction() {
+        // : Given
+        short baseValue = 16384;
+        short difference = 16383;
+        short expectedShort = 1;
+        // : When
+        short actualShort = ShortCalculator.subtract(baseValue, difference);
+        // : Then
+        assertEquals(expectedShort,actualShort);
+    }
+    
+    @Test
+    public void testShortMultiplication() {
+        // : Given
+        short multiplicand = 2;
+        short multiplier = 1;
+        short expectedShort = 2;
+        // : When
+        short actualShort = ShortCalculator.multiply(multiplicand, multiplier);
+        // : Then
+        assertEquals(expectedShort, actualShort);
+    }
+    
+    @Test
+    public void testShortDivision() {
+        // : Given
+        short dividend = 2;
+        short divisor = 1;
+        short expectedShort = 2;
+        // : When
+        short actualShort = ShortCalculator.divide(dividend, divisor);
+        // : Then
+        assertEquals(expectedShort, actualShort);
+    }
+
+    @Test
+    public void testShortRemainder() {
+        // : Given
+        short dividend = 2;
+        short divisor = 1;
+        short expectedShort = 0;
+        // : When
+        short actualShort = ShortCalculator.remainder(dividend, divisor);
+        // : Then
+        assertEquals(expectedShort, actualShort);
+    }
 }
+
